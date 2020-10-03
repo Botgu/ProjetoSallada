9b41aa5847d16afc060e9eaf1fb41966588b7c6e
yonlu <lss.emailbox@gmail.com>
2020-10-03 13:05:15 -0400

    chore: added link component to home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import api from "../../api";
-import { Link, useHistory } from "react-router-dom";
-import "../../styles.css";
+import { Link } from "react-router-dom";
 
 import { Container } from "./styles";
 
@@ -9,49 +8,28 @@ import Menu from "../../components/Menu/Menu";
 import Product from "../../components/Product/Product";
 
 const Home = () => {
-  const history = useHistory();
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     async function loadProducts() {
       const productList = await api.get("product");
       setProducts(productList.data.data.products);
-      console.log(productList.data.data.products[0].url);
     }
 
     loadProducts();
   }, []);
 
-  async function handleDetails(e) {
-    history.push(`/details/${e}`);
-  }
-
   return (
     <>
       <Menu title="Lista de produtos"></Menu>
       <Container>
-        {/* {product.map(p =>
-          <ul key={p.sku}>
-            <li>
-              <div className="containerProd">
-                <img src={p.images.medium} height="220px" alt="Vinho" />
-                <h4>{p.name}</h4>
-                <span onClick={e => (handleDetails(p.url))}> <span className="detalheButton">Detalhes</span></span>
-              </div>
-            </li>
-          </ul>
-        )} */}
-
         {products.map((product) => (
           <Product
             key={product.sku}
             image_url={product.images.medium}
             name={product.name}
           >
-            <span>
-              Detalhes
-              <Link to={`/${product.url}`}></Link>
-            </span>
+            <Link to={`/details/${product.url}`}>Detalhes</Link>
           </Product>
         ))}
       </Container>
