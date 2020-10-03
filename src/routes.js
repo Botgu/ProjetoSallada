6aa40b6f123b735bc07f52bd08be1d5e415bd14c
yonlu <lss.emailbox@gmail.com>
2020-10-03 17:31:09 -0400

    fix: starting on correct route when initializing app

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom';
-import Home from './Pages/Home/Home.js';
-import Details from './Pages/Details/Details';
-import NotFound from './Pages/NotFound/NotFound.js';
-
+import React from "react";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import Home from "./Pages/Home/Home.js";
+import Details from "./Pages/Details/Details";
+import NotFound from "./Pages/NotFound/NotFound.js";
 
 export default function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/details/:id" component={Details} />
-                <Route path="/404" component={NotFound} />
-                <Redirect to="/404" />
-            </Switch>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route exact path="/details/:id" component={Details} />
+        <Route exact path="/404" component={NotFound} />
+        <Redirect to="/404" />
+      </Switch>
+    </BrowserRouter>
+  );
 }
